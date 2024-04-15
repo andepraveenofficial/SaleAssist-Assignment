de27f46ed1ac4eb28ee57dd0a7b9f7191ca3e38b
andepraveenofficial <praveenande2023@gmail.com>
2024-04-15 10:11:58 +0530

    "Added SortProducts component to App layout"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import BagIcons from "./Components/BagIcons"
 import BagImages from "./Components/BagImages"
 import Header from "./Layouts/Header"
+import SortProducts from "./Layouts/SortProducts"
 
 const App = () => {
   return (
     <div className="bg-[#000] h-min-screen">
       <Header />
       <BagIcons />
+      <SortProducts />
       <BagImages />
     </div>
   )
---
+++ src/Layouts/SortProducts.jsx
@@ -0,0 +1,11 @@
+import { BiUpArrowAlt } from "react-icons/bi";
+const SortProducts = () => {
+    return (
+        <div className="flex flex-row justify-between p-2 m-2 ml-8 mr-8 text-white shadow-md shadow-slate-500">
+            <div>Bags : Backpacks</div>
+            <div className="flex items-center justify-center"><apan>13 products</apan> <BiUpArrowAlt className="size-10" /></div>
+        </div>
+    )
+}
+
+export default SortProducts
